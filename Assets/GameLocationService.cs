b0eab923dd5e6b57a1e07c9fddd01f5cf48fb55f
BxnSmxth02 <2002benjaminsmith@gmail.com>
2023-04-27 16:13:26 +0100

    V1
    All tasks "completed" - Ready for Hand-In

---
+++ Assets/GameLocationService.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLocationService : MonoBehaviour
+{
+    public IEnumerator Start()
+    {
+        if (!Input.location.isEnabledByUser) yield break;
+
+        Input.location.Start();
+
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait < 1)
+        {
+            Debug.Log("Timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            Debug.Log("location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+    }
+}
